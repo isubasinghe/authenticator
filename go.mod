69a4bf9f1704411ca9a01ac450ce2c92e0ad9964
isubasinghe <isubasinghe@student.unimelb.edu.au>
2020-07-08 21:04:12 +1000

    feat: added firebase verification

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module authenticator
 go 1.14
 
 require (
+	firebase.google.com/go/v4 v4.0.0
 	github.com/Netflix/go-env v0.0.0-20200512170851-5660fe1ab40a
 	github.com/fauna/faunadb-go v2.0.0+incompatible
+	google.golang.org/api v0.28.0
 )
--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
+	firebase "firebase.google.com/go/v4"
+	_ "firebase.google.com/go/v4/auth"
 	env "github.com/Netflix/go-env"
-	// "github.com/aws/aws-lambda-go/events"
-	// "github.com/aws/aws-lambda-go/lambda"
 	fauna "github.com/fauna/faunadb-go/faunadb"
+	"google.golang.org/api/option"
 )
 
 type environment struct {
-	FaunaSecret string `env:"FAUNADB_SECRET"`
+	FaunaSecret            string `env:"FAUNADB_SECRET"`
+	FirebaseSecretLocation string `env:"FIREBASE_SECRET_LOCATION"`
 }
 
 type token struct {
@@ -20,11 +23,13 @@ type token struct {
 
 var e environment
 var client *fauna.FaunaClient
+var app *firebase.App
+var ctx context.Context
 
-func loginUser(email string) (string, error) {
+func loginUser(uid string) (string, error) {
 	expr := fauna.Create(fauna.Tokens(), fauna.Obj{"instance": fauna.Select("ref",
 		fauna.Get(
-			fauna.MatchTerm(fauna.Index("users_by_email"), email),
+			fauna.MatchTerm(fauna.Index("users_by_uid"), uid),
 		),
 	)})
 	value, err := client.Query(expr)
@@ -40,17 +45,35 @@ func loginUser(email string) (string, error) {
 	return tokenStruct.FaunaToken, nil
 }
 
-// func handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-// }
+func getUID(idToken string) (string, error) {
+	msg := "nil"
+	client, err := app.Auth(ctx)
+	if err != nil {
+		return msg, err
+	}
+	token, err := client.VerifyIDTokenAndCheckRevoked(ctx, idToken)
+	if err != nil {
+		return msg, err
+	}
+	msg = token.UID
+	return msg, nil
+}
 
 func main() {
-	_, err := env.UnmarshalFromEnviron(&e)
+	var err error
+	_, err = env.UnmarshalFromEnviron(&e)
+
+	opt := option.WithCredentialsFile(e.FirebaseSecretLocation)
+	ctx = context.Background()
+	app, err = firebase.NewApp(ctx, nil, opt)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("KEY: ", e.FaunaSecret)
+	if err != nil {
+		log.Fatal(err)
+	}
 	client = fauna.NewFaunaClient(e.FaunaSecret)
-	loginUser("alice@site.example")
-	//lambda.Start(handler)
+
+	fmt.Println(loginUser("1234"))
+
 }
